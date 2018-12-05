4582117cb2a05ae4737c2edf51c0db9cabb5b54b
Olivia <oliviad3955@tywls.org>
2018-12-05 22:46:09 +0000

    conditional for assignment for user and computer

--- js/script.js
+++ js/script.js
@@ -9,8 +9,28 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+var userChoice = "";
+var computerChoice = "";
+var winner = "";
+var randomNumber = 0;
+
+
 $( document ).ready(function() {
-$("button").click(function() {
-	$("#userChoice").text($("#input").val());
+    $("button").click(function() {
+        userChoice = $("#input").val();
+    	$("#userChoice").text(userChoice);
+    	
+    	randomNumber = Math.random();
+    	console.log(randomNumber);
+    	if (randomNumber <= .3333) {
+             computerChoice = "rock";
+        } else if  (randomNumber <= .6666) {
+            computerChoice = "paper";
+        } else {
+            computerChoice = "scissors";
+        } 
+        console.log(computerChoice);
+        $("#computerChoice").text(computerChoice);
+    });
 });
-});
+
